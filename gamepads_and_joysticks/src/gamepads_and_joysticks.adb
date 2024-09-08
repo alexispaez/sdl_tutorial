6386f4abdb85a36180cb6309d68a25706dd833e8
alexispaez <jose.alexis.paez.thurgood@gmail.com>
2024-09-08 20:33:18 +0200

    Added joystick and gamepads

--- gamepads_and_joysticks/src/gamepads_and_joysticks.adb
+++ gamepads_and_joysticks/src/gamepads_and_joysticks.adb
@@ -1,14 +1,19 @@
 pragma Ada_2022;
 
+with Ada.Numerics; use Ada.Numerics;
+with Ada.Numerics.Long_Elementary_Functions;
+use Ada.Numerics.Long_Elementary_Functions;
 with Ada.Strings.UTF_Encoding;
 with Ada.Text_IO;
 with SDL; use SDL;
 with SDL.Events.Events;
+with SDL.Events.Joysticks; use SDL.Events.Joysticks;
 with SDL.Events.Keyboards;
 with SDL.Hints;
 with SDL.Images;
 with SDL.Images.IO;
 with SDL.Inputs.Joysticks; use SDL.Inputs.Joysticks;
+with SDL.Inputs.Joysticks.Makers;
 with SDL.Video.Rectangles;
 with SDL.Video.Renderers; use SDL.Video.Renderers;
 with SDL.Video.Renderers.Makers;
@@ -25,13 +30,12 @@ procedure Gamepads_And_Joysticks is
    Width  : constant := 640;
    Height : constant := 480;
 
-   Joystick_Dead_Zone : constant := 8000;
-
-   Window        : SDL.Video.Windows.Window;
-   Renderer      : SDL.Video.Renderers.Renderer;
-   Event         : SDL.Events.Events.Events;
-   Arrow_Texture : SDL.Video.Textures.Texture;
-   Joystick      : SDL.Inputs.Joysticks.Joystick;
+   Window            : SDL.Video.Windows.Window;
+   Renderer          : SDL.Video.Renderers.Renderer;
+   Event             : SDL.Events.Events.Events;
+   Arrow_Texture     : SDL.Video.Textures.Texture;
+   Joystick          : SDL.Inputs.Joysticks.Joystick;
+   Joystick_Instance : SDL.Inputs.Joysticks.Instances;
 
    procedure Load_Media
      (Texture   : in out SDL.Video.Textures.Texture;
@@ -80,9 +84,16 @@ procedure Gamepads_And_Joysticks is
    end Render;
 
    procedure Handle_Events is
+
+      Joystick_Dead_Zone : constant := 8000;
+
       Finished : Boolean := False;
+
+      X_Dir : Integer := 0;
+      Y_Dir : Integer := 0;
+
       Angle    : Long_Float := 0.0;
-      Flip_Type : SDL.Video.Renderers.Renderer_Flip := SDL.Video.Renderers.None;
+      Flip_Type : constant SDL.Video.Renderers.Renderer_Flip := SDL.Video.Renderers.None;
 
       use type Interfaces.C.int;
    begin
@@ -91,20 +102,40 @@ procedure Gamepads_And_Joysticks is
             case Event.Common.Event_Type is
                when SDL.Events.Quit =>
                   Finished := True;
+
+               when SDL.Events.Joysticks.Axis_Motion =>
+
+                  if Event.Joystick_Axis.Which = IDs (Joystick_Instance) then
+                     --  Motion on the first joystick
+                     if Event.Joystick_Axis.Axis = 0 then
+                        --  X axis motion
+                        if Event.Joystick_Axis.Value < -Joystick_Dead_Zone then
+                           --  Left of the dead zone
+                           X_Dir := -1;
+                        elsif Event.Joystick_Axis.Value > Joystick_Dead_Zone then
+                           --  Right of the dead zone
+                           X_Dir := 1;
+                        else
+                           X_Dir := 0;
+                        end if;
+                     elsif Event.Joystick_Axis.Axis = 1 then
+                        --  Y axis motion
+                        if Event.Joystick_Axis.Value < -Joystick_Dead_Zone then
+                           --  Below the dead zone
+                           Y_Dir := -1;
+                        elsif Event.Joystick_Axis.Value > Joystick_Dead_Zone then
+                           --  Above the dead zone
+                           Y_Dir := 1;
+                        else
+                           Y_Dir := 0;
+                        end if;
+                     end if;
+                  end if;
+
                when SDL.Events.Keyboards.Key_Down =>
                   case Event.Keyboard.Key_Sym.Key_Code is
                      when SDL.Events.Keyboards.Code_Escape =>
                         Finished := True;
-                     when SDL.Events.Keyboards.Code_A =>
-                        Angle := @ - 60.0;
-                     when SDL.Events.Keyboards.Code_D =>
-                        Angle := @ + 60.0;
-                     when SDL.Events.Keyboards.Code_Q =>
-                        Flip_Type := SDL.Video.Renderers.Horizontal;
-                     when SDL.Events.Keyboards.Code_W =>
-                        Flip_Type := SDL.Video.Renderers.None;
-                     when SDL.Events.Keyboards.Code_E =>
-                        Flip_Type := SDL.Video.Renderers.Vertical;
                      when others => null;
                   end case;
                when others => null;
@@ -116,6 +147,13 @@ procedure Gamepads_And_Joysticks is
          Renderer.Clear;
 
          --  Render arrow
+         if Y_Dir /= 0 or else X_Dir /= 0 then
+            Angle := Arctan (Long_Float (Y_Dir), Long_Float (X_Dir)) *
+              (180.0 / Pi);
+         else
+            Angle := 0.0;
+         end if;
+
          Render (Renderer,
                          Arrow_Texture,
                          (Width - Arrow_Texture.Get_Size.Width) / 2,
@@ -138,10 +176,18 @@ begin
 
    SDL.Hints.Set (SDL.Hints.Render_Scale_Quality, "1");
 
+   --  Check for the presence of joysticks
    if SDL.Inputs.Joysticks.Total < 1 then
-      Ada.Text_IO.Put_Line ("No joysticks connected");
+      Ada.Text_IO.Put_Line ("No joysticks connected! Exiting.");
       return;
+   else
+      Ada.Text_IO.Put_Line ("Found " &
+                              SDL.Inputs.Joysticks.Total'Image &
+                           " joysticks.");
    end if;
+   --  Open the first joystick
+   SDL.Inputs.Joysticks.Makers.Create (1, Joystick);
+   Joystick_Instance := SDL.Inputs.Joysticks.Instance (Joystick);
 
    if not SDL.Images.Initialise (Flags => SDL.Images.Enable_PNG) then
       return;
