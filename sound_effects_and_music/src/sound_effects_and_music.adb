be0b2f2f5f3366b643fab58d737a19dab1d0b889
Jose Alexis Paez <jose.alexis.paez@keyloop.com>
2024-09-11 20:03:44 +0200

    Added sound effects tutorial

--- sound_effects_and_music/src/sound_effects_and_music.adb
+++ sound_effects_and_music/src/sound_effects_and_music.adb
@@ -1,55 +1,101 @@
+pragma Ada_2022;
+
+with Ada.Strings.UTF_Encoding;
 with Ada.Text_IO;
 with SDL.Events.Events;
 with SDL.Events.Keyboards;
+with SDL.Hints;
+with SDL.Images;
+with SDL.Images.IO;
+with SDL.Mixer;
+with SDL.Mixer.Music;
+use SDL.Mixer.Music;
+with SDL.Video.Rectangles;
+with SDL.Video.Renderers; use SDL.Video.Renderers;
+with SDL.Video.Renderers.Makers;
 with SDL.Video.Surfaces;
-with SDL.Video.Surfaces.Makers;
+with SDL.Video.Textures;
+with SDL.Video.Textures.Makers;
 with SDL.Video.Windows.Makers;
+use SDL;
+with Interfaces.C; use Interfaces.C;
 
 procedure Sound_Effects_And_Music is
 
-   type Key_Press_Surface_Index is
-     (Default,
-      Up,
-      Down,
-      Left,
-      Right);
-   type Key_Press_Surfaces is array (Key_Press_Surface_Index) of
-     SDL.Video.Surfaces.Surface;
+   Width  : constant := 640;
+   Height : constant := 480;
 
-   Width           : constant := 640;
-   Height          : constant := 480;
-   Surfaces        : Key_Press_Surfaces;
-   Window_Surface  : SDL.Video.Surfaces.Surface;
-   Current_Surface : SDL.Video.Surfaces.Surface;
-   Window          : SDL.Video.Windows.Window;
-   Event           : SDL.Events.Events.Events;
+   Window   : SDL.Video.Windows.Window;
+   Renderer : SDL.Video.Renderers.Renderer;
+   Event    : SDL.Events.Events.Events;
 
-   procedure Load_Media (Surfaces : in out Key_Press_Surfaces) is
-      Surface : SDL.Video.Surfaces.Surface;
+   Texture  : SDL.Video.Textures.Texture;
+   Music    : SDL.Mixer.Music.Music_Type;
+   --  Music : SDL.Mixer.Music.;
+
+   procedure Load_Media is
+
+      package UTF_Strings renames Ada.Strings.UTF_Encoding;
+
+      procedure Load_Texture
+        (Texture        : in out SDL.Video.Textures.Texture;
+         Renderer       : SDL.Video.Renderers.Renderer;
+         File_Name      : UTF_Strings.UTF_String) is
+         Loaded_Surface : SDL.Video.Surfaces.Surface;
+      begin
+         SDL.Images.IO.Create (Loaded_Surface, File_Name);
+
+         Loaded_Surface.Set_Colour_Key ((0, 255, 255, 255), True);
+
+         SDL.Video.Textures.Makers.Create
+           (Texture,
+            Renderer,
+            Loaded_Surface);
+
+         Loaded_Surface.Finalize;
+      end Load_Texture;
    begin
-      SDL.Video.Surfaces.Makers.Create (Surface,
-                                        "../resources/press.bmp");
-      Surfaces (Default) := Surface;
-      SDL.Video.Surfaces.Makers.Create (Surface,
-                                        "../resources/up.bmp");
-      Surfaces (Up) :=  Surface;
-      SDL.Video.Surfaces.Makers.Create (Surfaces (Down),
-                                        "../resources/down.bmp");
-      SDL.Video.Surfaces.Makers.Create (Surfaces (Left),
-                                        "../resources/left.bmp");
-      SDL.Video.Surfaces.Makers.Create (Surfaces (Right),
-                                        "../resources/right.bmp");
+      Load_Texture (Texture,
+                    Renderer,
+                    "../resources/prompt.png");
+      SDL.Mixer.Music.Load ("../resources/beat.wav", Music);
    end Load_Media;
 
-   procedure Free_Media (Surfaces : in out Key_Press_Surfaces) is
+   procedure Render
+     (Renderer         : in out SDL.Video.Renderers.Renderer;
+      Texture          : in out SDL.Video.Textures.Texture;
+      X                : SDL.Dimension;
+      Y                : SDL.Dimension;
+      Angle            : Long_Float;
+      Flip_Type        : SDL.Video.Renderers.Renderer_Flip) is
+
+      To_Rectangle : constant SDL.Video.Rectangles.Rectangle :=
+        (X,
+         Y,
+         Texture.Get_Size.Width,
+         Texture.Get_Size.Height);
+      Center_Point     : constant SDL.Video.Rectangles.Point :=
+        (Texture.Get_Size.Width / 2,
+         Texture.Get_Size.Height / 2);
    begin
-      for S of Surfaces loop
-         S.Finalize;
-      end loop;
+      Renderer.Copy_To (Texture,
+                     To_Rectangle,
+                     Angle,
+                     Center_Point,
+                     Flip_Type);
+
+   end Render;
+
+   procedure Free_Media is
+   begin
+      null;
    end Free_Media;
 
    procedure Handle_Events is
       Finished : Boolean := False;
+
+      Angle     : Long_Float := 0.0;
+      Flip_Type : constant SDL.Video.Renderers.Renderer_Flip := SDL.Video.Renderers.None;
    begin
       loop
          while SDL.Events.Events.Poll (Event) loop
@@ -60,50 +106,75 @@ procedure Sound_Effects_And_Music is
                   case Event.Keyboard.Key_Sym.Key_Code is
                      when SDL.Events.Keyboards.Code_Escape =>
                         Finished := True;
-                     when SDL.Events.Keyboards.Code_Up =>
-                        Current_Surface := Surfaces (Up);
-                     when SDL.Events.Keyboards.Code_Down =>
-                        Current_Surface := Surfaces (Down);
-                     when SDL.Events.Keyboards.Code_Left =>
-                        Current_Surface := Surfaces (Left);
-                     when SDL.Events.Keyboards.Code_Right =>
-                        Current_Surface := Surfaces (Right);
+                     when SDL.Events.Keyboards.Code_9 =>
+                        SDL.Mixer.Music.Play (Music, -1);
                      when others => null;
                   end case;
                when others => null;
             end case;
          end loop;
 
-         Window_Surface.Blit (Current_Surface);
-         Window.Update_Surface;
+         --  Clear screen
+         Renderer.Set_Draw_Colour ((255, 255, 255, 255));
+         Renderer.Clear;
+
+         --  Render arrow
+         Angle := 0.0;
+
+         Render (Renderer,
+                 Texture,
+                 (Width - Texture.Get_Size.Width) / 2,
+                 (Height - Texture.Get_Size.Height) / 2,
+                 Angle,
+                 Flip_Type);
+
+         Renderer.Present;
 
          exit when Finished;
       end loop;
    end Handle_Events;
 
 begin
-   if not SDL.Initialise (Flags => SDL.Enable_Screen) then
+   if not SDL.Initialise (Flags =>
+                            SDL.Enable_Screen or
+                              SDL.Enable_Audio)
+   then
       return;
    end if;
 
+   SDL.Hints.Set (SDL.Hints.Render_Scale_Quality, "1");
+
    SDL.Video.Windows.Makers.Create
      (Win      => Window,
-      Title    => "SDL Tutorial - Key Presses",
+      Title    => "SDL Tutorial - Sound effects and Music",
       Position => SDL.Natural_Coordinates'(X => 20, Y => 20),
       Size     => SDL.Positive_Sizes'(Width, Height),
-      Flags    => 0);
+      Flags    => SDL.Video.Windows.Shown);
 
-   Window_Surface := Window.Get_Surface;
+   SDL.Video.Renderers.Makers.Create
+     (Window => Window,
+      Rend   => Renderer,
+      Flags  => SDL.Video.Renderers.Accelerated or
+        SDL.Video.Renderers.Present_V_Sync);
 
-   Load_Media (Surfaces);
+   Renderer.Set_Draw_Colour ((others => 255));
 
-   Current_Surface := Surfaces (Default);
+   if not SDL.Images.Initialise (Flags => SDL.Images.Enable_PNG) then
+      return;
+   end if;
+
+   SDL.Mixer.Open (SDL.Mixer.Default_Frequency,
+                   SDL.Mixer.Default_Format,
+                   2,
+                   2048);
+
+   Load_Media;
 
    Handle_Events;
 
-   Free_Media (Surfaces);
-   Window_Surface.Finalize;
+   Free_Media;
    Window.Finalize;
+   SDL.Images.Finalise;
    SDL.Finalise;
 
    Ada.Text_IO.Put_Line ("Process complete.");
